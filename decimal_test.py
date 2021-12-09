9727f0643a9944dcfbb9def7152aebe0b05a6b11
Mark Andrew Miller <MAM@lbl.gov>
2021-12-09 11:26:27 -0500

    force PyCharm to show print results
    and the pointof the test is to illustrate that cm.Observation(observation_type=cm.CodeableConcept(), value_decimal=1.23) can't be serialized to YAML

--- decimal_test.py
+++ decimal_test.py
@@ -3,11 +3,15 @@ from linkml_runtime.dumpers import yaml_dumper
 
 
 def test_dumps_vcc():
-    vcc_obs = cm.Observation(observation_type=cm.CodeableConcept(), value_codeable_concept=cm.CodeableConcept())
+    vcc_obs = cm.Observation(observation_type=cm.CodeableConcept(),
+                             value_codeable_concept=cm.CodeableConcept(
+                                 coding=cm.Coding(code="3.1415", system="math professor")))
     vcc_dump = yaml_dumper.dumps(vcc_obs)
     # print statements ignored by default in passing pytests?
     # https://stackoverflow.com/questions/24617397/how-to-print-to-console-in-pytest
-    # try -s argument or PyCharm specific configuration?
+    # https://intellij-support.jetbrains.com/hc/en-us/community/posts/360007644040-Show-logging-output-when-using-pytest
+    # added `-p no:logging -s` to "Additional Arguments" in Run Configuration
+    # also try (global?) Run Configuration Template for pytest
     print(vcc_dump)
     assert len(vcc_dump) > 0
 
